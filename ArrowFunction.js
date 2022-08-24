2ab5a2cc26788d457e2eeef8a265b8c3b0bff425
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-08-24 13:26:57 +0500

    Update ArrowFunction.js

--- ArrowFunction.js
+++ ArrowFunction.js
@@ -1,1 +1,23 @@
+// Arrow function is the new and shortes way to declare functions in javascript the arrwo function was introduced in the ES6 version.
+// Before Arrow function we write our functions in this way.
+
+function fullname(fname,lname){
+  document.write(fname + lname); 
+};
+
+fullname("Muhammad","Shakir");
+
+// We also write functions in the other way
+
+let fullname = function(fname,lname){
+document.write(fname + lname)
+};
+
+fullname("Shafiqa " + " Iqbal");
+
+
+// But when arrow function comes
+
+let fullname = (fname,lname) => document.write(fname,lname)
+fullname("Shafiqa","Iqbal");
 
