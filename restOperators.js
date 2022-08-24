8c8fc7725b1e2ee284725141e08351593137b3d5
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-08-24 14:05:31 +0500

    Update restOperators.js

--- restOperators.js
+++ restOperators.js
@@ -1,1 +1,12 @@
+//Rest operator are super usefull when you have multiple arrguments in you functions.
 
+const std = (name,course,...marks) => {
+let std = 0;
+for(let i in marks){
+std += marks[i];
+}
+document.write(`${name} of ${course} taken total marks : ${std}`);
+}
+
+std("Muhammad Shakir","Software Eng",90,88,76,90,90,90);
+std("Anum Mustafa","Software Eng",90,88,76,90,80,80);
