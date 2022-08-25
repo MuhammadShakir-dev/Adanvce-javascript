171aaf7f5ea01774b6c39953fae2047bdaaa75bf
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-08-25 14:54:56 +0500

    Update spread.js

--- spread.js
+++ spread.js
@@ -1,1 +1,15 @@
+// spread operator are samae as rest operatores .
+// spread opreator sperate array into differents arrguments and then perfom operations on it.
+// speard operator spread an array into multiple arrguments.
+// spread operator will always declear at tha calling of a function.
+// the main difference between the reet and spread operator is that .
+// rest operator assign or dclear as a parameter in a function.
+// spread operator will assign at the calling of a function.
+// you can also add or remove any value to an array  as well as in an objects using spread operator.
+// you can concate arrays and objects using spread operators.
+
+// lets strart practice   
+
+
+
 
