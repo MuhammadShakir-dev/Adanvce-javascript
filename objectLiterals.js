155fef2f6deb6f6fb0069e582e3bf22bfbd677bf
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-08-25 23:58:28 +0500

    Update objectLiterals.js

--- objectLiterals.js
+++ objectLiterals.js
@@ -1,1 +1,29 @@
+// Lets learn how to create objects in javascript ES6 version.
 
+// the old method of creating objects before ES6.
+let name = "Muhammad Shakir";
+let studentName ={
+name : name,
+}
+document.write(studentName.name);
+
+// shorter way .
+// this way is only works of your property name and your key name both are same.
+let name = "Muhammad Shakir";
+let studentName ={
+name,
+}
+document.write(studentName.name);
+
+
+//lets take an other example.
+let stdName = "Muhammad Shakir";
+let stdDesg = "Co Lead";
+let stdComm = "Google developer Student Clubs";
+let stdDetials = {
+    stdName,
+    stdDesg,
+    stdComm,
+}
+//output.
+{stdName: 'Muhammad Shakir', stdDesg: 'Co Lead', stdComm: 'Google developer Student Clubs'}
